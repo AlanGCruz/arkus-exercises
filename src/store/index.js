105993324fe7d4dbfede29128130cb6dc4e24e59
Alan Cruz <alan.cruz@protonmail.com>
2019-11-22 10:56:59 -0800

    add the createdUsers: and editedUsers: state, getter and mutations to the vuex index.js file

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    createdUsers: 0,
+    editedUsers: 0,
+  },
+  getters: {
+    created: state => {
+      return state.createdUsers;
+    },
+    edited: state => {
+      return state.editedUsers;
+    },
   },
   mutations: {
+    incrementCreated: state => {
+      state.createdUsers++;
+    },
+    incrementEdited: state => {
+      state.editedUsers++;
+    },
   },
   actions: {
   },
