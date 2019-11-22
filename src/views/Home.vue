62bf89580993ee836c233a0723a14f6896338813
Alan Cruz <alan.cruz@protonmail.com>
2019-11-21 22:15:15 -0800

    add vuejs files and directories

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ContactList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ContactList from '@/components/ContactList.vue'
+
+export default {
+  name: 'home',
+  components: {
+    ContactList
+  }
+}
+</script>
